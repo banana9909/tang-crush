00f157bfb32f970179e2e934d06defa5896ed22f
banana9909 <88871133+banana9909@users.noreply.github.com>
2021-08-13 11:25:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
     introTitle: 'Babe à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    Mà sao Híu cứ dễ thương như nàyy`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    title: 'Phải chăng em đã hihi ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã okela ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thíchhh <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại ỏ ỏ anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    reply: 'tại tui dễ thương <33333333',
+    mess: 'Hoàng biết mà 🥰. ỏ ỏ nhiều nhiều 😘😘',
+    messDesc: 'hẹn 1 ngày gặp nhauuu',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.instagram.com/__minhoang_/?hl=en' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
